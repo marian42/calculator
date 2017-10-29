80af04b67f9dbe8d46666607eebbfc487ca7a42b
Marian <mail@marian42.de>
2017-10-29 22:22:07 +0100

    Minify resulting javascript code

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
+const Uglify = require("uglifyjs-webpack-plugin");
 
 module.exports = {
-  entry: './build/script.js',
-  output: {
-    path: path.resolve(__dirname),
-    filename: 'script.js'
-  }
+	entry: './build/script.js',
+	output: {
+		path: path.resolve(__dirname),
+		filename: 'script.js'
+	},
+	plugins: [
+		new Uglify()
+	]
 };
