68f707382b73f1851a964c6cdde8428241d830b3
Marian <mail@marian42.de>
2017-10-31 14:39:23 +0100

    Better number formatting

--- ts/calculator/Result.ts
+++ ts/calculator/Result.ts
@@ -1,3 +1,5 @@
+import { TinyNumber } from "../language/TinyNumber";
+
 export class Result {
 	public readonly value: number;
 
@@ -6,6 +8,47 @@ export class Result {
 	}
 
 	public toString(): string {
-		return this.value.toString();
+		if (this.value == Number.POSITIVE_INFINITY) {
+			return "∞";
+		} else if (this.value == Number.NEGATIVE_INFINITY) {
+			return "-∞";
+		} else if (this.value == Number.NaN) {
+			return "not a number";
+		} else if (Math.abs(this.value) < Number.EPSILON) {
+			return "0";
+		}
+		return Result.formatNumber(this.value);
+	}
+
+	private static numberToString(value: number, maxDecimalPoints = 8): string {
+		var lower = Math.floor(value);
+		var upper = Math.ceil(value);
+		if (Math.abs(value - lower) < Number.EPSILON) {
+			value = lower;
+		} else if (Math.abs(value - upper) < Number.EPSILON) {
+			value = upper;
+		}
+		var result = value.toString();
+		if (result.indexOf('.') != -1) {
+			var firstSignificantDigit = result.indexOf('.') + 1;
+			while (firstSignificantDigit < result.length && result.charAt(firstSignificantDigit) == '0') {
+				firstSignificantDigit++;
+			}
+			if (firstSignificantDigit + maxDecimalPoints < result.length) {
+				result = result.substr(0, firstSignificantDigit + maxDecimalPoints);
+			}
+		}
+		return result;
+	}
+
+	private static formatNumber(value: number): string {
+		const maxExponent = 6;
+		const minExponent = -6;
+		let exponent = Math.floor(Math.log10(value));
+		if (exponent < minExponent || exponent > maxExponent) {
+			var base = value / Math.pow(10, exponent);
+			return Result.numberToString(base, 4) + "⨯10" + TinyNumber.create(exponent)
+		}
+		return Result.numberToString(value);
 	}
 }
